a54b8e600a4fe60622c74f00536c93ba968c8e9f
wym <yaomin.wym@gmail.com>
2017-05-13 01:37:34 +0800

    add feature

---
+++ js/api.js
@@ -0,0 +1,9 @@
+HOST = "http://120.27.6.10:8349/";
+HOST_API = HOST+"bankMaintenance-api//bank/api/";
+HOST_USER = HOST_API+"user/";
+HOST_ORDER = HOST_API+"e/order/";
+
+USER_INFO_API = HOST_USER+"banker_info"
+
+ORDER_LIST_API = HOST_USER+"list"
+
--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
-plus.webview.currentWebview().addEventListener('show', function() {
-	var current = plus.webview.currentWebview().id;
+self = plus.webview.currentWebview()
+self.addEventListener('show', function() {
+	var current = self.id;
 	if(['index_recive.html', 'index_order.html', 'index_check.html', 'index_message.html', 'index_ucenter.html'].indexOf(current) >= 0) {
 		var main = plus.webview.getLaunchWebview();
 		console.log('show ' + current.id);
@@ -9,7 +10,12 @@ plus.webview.currentWebview().addEventListener('show', function() {
 	}
 });
 mui('body').on('tap', 'a,li,div', function(e) {
-	var targetTab = this.getAttribute('link');
+	var targetTab_url = this.getAttribute('link');
+	if(!targetTab_url){
+		return;
+	}
+	targetTab = targetTab_url.split("?")[0];
+	var params = parseQueryString(targetTab_url);
 	if(targetTab != null) {
 		console.log('link = ' + targetTab);
 		var page = plus.webview.getWebviewById(targetTab);
@@ -20,7 +26,8 @@ mui('body').on('tap', 'a,li,div', function(e) {
 				id: targetTab,
 				styles: {
 					top: '0px'
-				}
+				},
+				extras:params
 			});
 		} else {
 			console.log('page ' + targetTab + ' is loaded');
@@ -30,8 +37,10 @@ mui('body').on('tap', 'a,li,div', function(e) {
 	return true;
 });
 
-function redirect(pageUrl) {
-	console.log('link = ' + pageUrl);
+function redirect(pageFullUrl) {
+	console.log('link = ' + pageFullUrl);
+	pageUrl = pageFullUrl.split("?")[0];
+	var params = parseQueryString(pageFullUrl);
 	var page = plus.webview.getWebviewById(pageUrl);
 	if(!page) {
 		console.log('open new page ' + pageUrl);
@@ -40,10 +49,73 @@ function redirect(pageUrl) {
 			id: pageUrl,
 			styles: {
 				top: '0px'
-			}
+			},
+			extras:params
 		});
 	} else {
 		console.log('page ' + pageUrl + ' is loaded');
 		page.show("slide-in-right", 300);
 	}
+}
+bank = {
+	"post":function(url, data, callback) {
+		mui.ajax({
+			url: url,
+			type: 'post',
+			data: data,
+			dataType: 'json',
+			success: function(res) {
+				if(res.code == 200) {
+					if(callback) {
+						callback(res.data);
+					}
+				} else {
+					mui.toast(res.msg);
+				}
+			},
+			error: function() {
+				mui.toast('网络错误');
+			}
+		})
+	},
+	"get":function(url, params, callback) {
+		mui.ajax({
+			url: url,
+			type: 'get',
+			data: params,
+			dataType: 'json',
+			success: function(res) {
+				if(res.code == 200) {
+					if(callback) {
+						callback(res.data);
+					}
+				} else {
+					mui.toast(res.msg);
+				}
+			},
+			error: function() {
+				mui.toast('网络错误');
+			}
+		})
+	}
+}
+
+function GetQueryString(name)
+{
+     var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+     var r = window.location.search.substr(1).match(reg);
+     if(r!=null)return  unescape(r[2]); return null;
+}
+function parseQueryString(url) {
+ var reg_url = /^[^\?]+\?([\w\W]+)$/,
+  reg_para = /([^&=]+)=([\w\W]*?)(&|$|#)/g,
+  arr_url = reg_url.exec(url),
+  ret = {};
+ if (arr_url && arr_url[1]) {
+  var str_para = arr_url[1], result;
+  while ((result = reg_para.exec(str_para)) != null) {
+   ret[result[1]] = result[2];
+  }
+ }
+ return ret;
 }
